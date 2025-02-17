06d888373d2f14e6271ee622dcbc6d5f746423df
ali imangholi <a.imangholi@deltacenter.local>
2025-02-17 20:17:56 +0330

    work on backend

---
+++ data/backend-practice.js
@@ -0,0 +1,12 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+    // document.querySelector('html').innerHTML = xhr.response;
+    console.log(xhr.response);
+});
+
+xhr.open(
+    'GET',
+    'https://supersimplebackend.dev/images/apple.jpg'
+);
+xhr.send();
